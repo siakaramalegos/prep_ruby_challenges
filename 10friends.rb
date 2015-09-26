063e3b63192311e71fc058db7b1a4b45a1f346b3
Sia <siakaramalegos@gmail.com>
2015-09-26 16:21:50 -0500

    Converted 10friends to accept number of friends and count as arguments

--- 10friends.rb
+++ 10friends.rb
@@ -9,40 +9,47 @@
 # number 100. For this one, be as specific as possible to actually write out the
 # logic of the procedure.
 
-def correct_index(index)
+def correct_index(index, number_friends)
   if index == -2
-    index = 8
-  elsif index == 10
+    index = number_friends - 2
+  elsif index == number_friends
     index = 0
   end
   index
 end
 
-friends = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-count = 1
-direction = 1
-current_friend_index = 0
-puts "friend #{friends[current_friend_index]} says #{count}, index is #{current_friend_index}, direction is #{direction}"
-
-while count <= 100
-  if count == 1
-    count += 1
-    next
-  elsif count % 7 == 1 && count % 11 == 1
-    # reverse and skip
-    direction *= -1
-    current_friend_index = correct_index(current_friend_index + direction * 2)
-  elsif count % 7 == 1
-    # reverse
-    direction *= -1
-    current_friend_index = correct_index(current_friend_index + direction)
-  elsif count % 11 == 1
-    # skip
-    current_friend_index = correct_index(current_friend_index + direction * 2)
-  else
-    # continue
-    current_friend_index = correct_index(current_friend_index + direction)
+def counting_game(number_friends=10, max_count=100)
+  friends = []
+  number_friends.times do |friend|
+    friends << friend + 1
   end
+  count = 1
+  direction = 1
+  current_friend_index = 0
   puts "friend #{friends[current_friend_index]} says #{count}, index is #{current_friend_index}, direction is #{direction}"
-  count += 1
+
+  while count <= max_count
+    if count == 1
+      count += 1
+      next
+    elsif count % 7 == 1 && count % 11 == 1
+      # reverse and skip
+      direction *= -1
+      current_friend_index = correct_index(current_friend_index + direction * 2, number_friends)
+    elsif count % 7 == 1
+      # reverse
+      direction *= -1
+      current_friend_index = correct_index(current_friend_index + direction, number_friends)
+    elsif count % 11 == 1
+      # skip
+      current_friend_index = correct_index(current_friend_index + direction * 2, number_friends)
+    else
+      # continue
+      current_friend_index = correct_index(current_friend_index + direction, number_friends)
+    end
+    puts "friend #{friends[current_friend_index]} says #{count}, index is #{current_friend_index}, direction is #{direction}"
+    count += 1
+  end
 end
+
+counting_game(5, 15)
