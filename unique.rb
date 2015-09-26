3c73d21e6df8ee96c90c8248b1af694dca781fab
Sia <siakaramalegos@gmail.com>
2015-09-26 18:53:48 -0500

    Fixed uniques for other cases

--- unique.rb
+++ unique.rb
@@ -5,11 +5,15 @@
 # => [1,5,”frog”,2,3]
 
 def uniques items
-  items.each_with_index do |item, index|
-    next if index == 0
-    range_max = index -1
-    if items[0..range_max].include?(item)
+  # don't want to compare the first item so start at 1
+  index = 1
+  # changed from .each and .each_with_index because removing items was mixing up the item and index
+  while index + 1 <= items.length do
+    range_max = index - 1
+    if items[0..range_max].include?(items[index])
       items.delete_at(index)
+    else
+      index += 1
     end
   end
   items
